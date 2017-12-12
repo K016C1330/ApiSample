48c575296f7415c41eb4738320668ff1e2db7e42
Uehara Yoyogi <besthato@gmail.com>
2017-12-12 12:03:31 +0900

    ファーストコミット

--- ApiSample/Controllers/ValuesController.cs
+++ ApiSample/Controllers/ValuesController.cs
@@ -5,7 +5,7 @@ using System.Net;
 using System.Net.Http;
 using System.Web.Http;
 
-namespace ApiSample.Controllers
+namespace ApiSample.Controllers.Apis
 {
     [Authorize]
     public class ValuesController : ApiController
